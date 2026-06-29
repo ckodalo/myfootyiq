11c7b9e54e27fb671b6a523ff46cf8b97bd7f5a0
ckodalo <ckodalo@gmail.com>
2026-06-30 00:25:10 +0400

    fetchOdds

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,16 @@ from fastapi import FastAPI
 
 from routes.football_data import router as football_data_router
 from routes.odds import router as odds_router
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # tighten for production
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(football_data_router)
 app.include_router(odds_router)
